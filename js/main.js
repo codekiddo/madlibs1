a435907adef109446167863c886633f450155ed6
Lukas Weinwurm <lweinwurm@hotmail.com>
2014-05-27 17:46:55 +0200

    refactor ui

--- js/main.js
+++ js/main.js
@@ -1,16 +1,22 @@
 angular.module('myApp', [])
   .controller('MainCtrl', function($scope) {
-    $scope.data = { 
-      name: 'name',
-      dirty_task:'dirty task',
-      obnoxiuous_celebrity:'obnoxiuous celebrity',
-      job_title:'job title',
-      celebrity:'celebrity',
-      huge_number:'huge number',
-      tedious_task:'tedious task',
-      useless_skill:'useless skill',
-      adjective:'adjective'
-    };
+
 
     $scope.gender = "male";
+
+    $scope.submit = function() {
+      if ($scope.myForm.$error.required) {
+        $scope.submitted = false;
+        $scope.error = true;
+      } else {
+        $scope.submitted = true;
+        $scope.reset = false;
+        $scope.error = false;
+      }
+    };
+
+    $scope.startOver = function() {
+      $scope.submitted = false;
+      $scope.data = null;
+    };
   });
