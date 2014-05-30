b57ea69fe2bb9e92a19e386976adf86ea42abe99
Lukas Weinwurm <lweinwurm@hotmail.com>
2014-05-30 14:04:41 +0200

    refactor

--- js/main.js
+++ js/main.js
@@ -4,6 +4,20 @@ angular.module('myApp', [])
 
     $scope.gender = "male";
 
+    $scope.toggleGender = function() {
+      $scope.gender = $scope.gender == "male" ? "female" : "male";
+    }
+
+    $scope.$watch("gender", function() {
+      if ($scope.gender == "male") {
+        $scope.he_she = "she";
+        $scope.his_her = "her";
+      } else {
+        $scope.he_she = "he";
+        $scope.his_her = "his";
+      }
+    });
+
     $scope.submit = function() {
       if ($scope.myForm.$error.required) {
         $scope.submitted = false;
