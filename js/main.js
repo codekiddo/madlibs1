9f48343baa320d79d3cebdc2b4e14382a62c817a
Lukas Weinwurm <lweinwurm@hotmail.com>
2014-06-03 19:33:40 +0200

    add more animation

--- js/main.js
+++ js/main.js
@@ -1,20 +1,18 @@
-angular.module('myApp', [])
+angular.module('myApp', ['ngAnimate'])
   .controller('MainCtrl', function($scope) {
 
 
     $scope.gender = "male";
 
-    $scope.toggleGender = function() {
-      $scope.gender = $scope.gender == "male" ? "female" : "male";
-    }
-
     $scope.$watch("gender", function() {
       if ($scope.gender == "male") {
         $scope.he_she = "she";
         $scope.his_her = "her";
+        $scope.him_her = "her";
       } else {
         $scope.he_she = "he";
         $scope.his_her = "his";
+        $scope.him_her = "him";
       }
     });
 
@@ -27,6 +25,10 @@ angular.module('myApp', [])
         $scope.reset = false;
         $scope.error = false;
       }
+    };
+
+    $scope.toggleGender = function() {
+      $scope.gender = $scope.gender == "male" ? "female" : "male";
     };
 
     $scope.startOver = function() {
